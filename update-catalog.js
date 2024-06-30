078e3bd3fadd957ba76b33fdc27fbcd9072ef84b
Said Lebbihi <said@ams-xlab.com>
2024-06-30 16:01:47 +0200

    use updateMany using products array as stream and deleteMany

--- update-catalog.js
+++ update-catalog.js
@@ -7,6 +7,9 @@ const { timing } = require('./helpers/timing');
 const { Metrics } = require('./helpers/metrics');
 const { Product } = require('./product');
 
+const _ = require('lodash');
+const { Readable } = require('stream');
+
 const MONGO_URL = 'mongodb://localhost:27017/test-product-catalog';
 const catalogUpdateFile = 'updated-catalog.csv';
 
@@ -21,6 +24,24 @@ async function main() {
       () => updateDataset(db)));
 }
 
+
+function arrayToStream(array, chunkSize) {
+  let index = 0;
+
+  return new Readable({
+    objectMode: true,
+    read() {
+      if (index < array.length) {
+        const chunk = array.slice(index, index + chunkSize);
+        index += chunkSize;
+        this.push(chunk);
+      } else {
+        this.push(null); // Signal end of stream
+      }
+    }
+  });
+}
+
 async function updateDataset(db) {
   const csvContent = fs.readFileSync(catalogUpdateFile, 'utf-8');
   const rowsWithHeader = csvContent.split('\n');
@@ -29,44 +50,46 @@ async function updateDataset(db) {
   const metrics = Metrics.zero();
   function updateMetrics(updateResult) {
     if (updateResult.modifiedCount) {
-      metrics.updatedCount += 1;
+      metrics.updatedCount += updateResult.modifiedCount;
     }
     if (updateResult.upsertedCount) {
-      metrics.addedCount += 1;
+      metrics.addedCount += updateResult.upsertedCount;
     }
   }
 
   const dbCatalogSize = await db.collection('Products').count();
-  const closestPowOf10 = 10**(Math.ceil(Math.log10(dbCatalogSize)));
+  const closestPowOf10 = 10 ** (Math.ceil(Math.log10(dbCatalogSize)));
   function logProgress(nbCsvRows) {
     const progressIndicator = nbCsvRows * 100 / closestPowOf10;
-    if (progressIndicator%10 === 0) {
+    if (progressIndicator % 10 === 0) {
       console.debug(`[DEBUG] Processed ${nbCsvRows} rows...`);
     }
   }
 
-  const products = dataRows.filter(dataRow => dataRow).map(row => Product.fromCsv(row));
-  await Promise.map(products, async (product, i) => {
-    const updateResult = await db.collection('Products')
-      .updateOne(
-        { _id: product._id },
-        { $set: product },
-        { upsert: true });
-    updateMetrics(updateResult);
-    logProgress(i);
-  });
-
-  const dbIds = (await db.collection('Products').find({}, {_id: 1}).toArray()).map(o => o._id);
-  const isDeletedId = id => !products.find(p => p._id === id);
-  const deletedProductIds = dbIds.filter(id => isDeletedId(id));
-  for (const pId of deletedProductIds) {
-    const deleteResult = await db.collection('Products').deleteOne({_id: pId});
-    if (deleteResult.deletedCount) {
-      metrics.deletedCount += 1;
-    }
+  const allProducts = dataRows.filter(dataRow => dataRow).map(row => Product.fromCsv(row));
+  const stream = arrayToStream(allProducts, 1000);
+  let index = 0;
+  for await (const products of stream) {
+    await Promise.map(products, async (product) => {
+      const updateResult = await db.collection('Products')
+        .updateOne(
+          { _id: product._id },
+          { $set: product },
+          { upsert: true });
+      updateMetrics(updateResult);
+      logProgress(++index);
+    });
   }
 
-  logMetrics(dataRows.length-1, metrics);// dataRows.length-1 because there is a new line at the end of file.
+  const dbIds = (await db.collection('Products').find({}, { _id: 1 }).toArray()).map(o => o._id);
+  const productIds = allProducts.map(product => product._id);
+  const deletedProductIds = _.difference(dbIds, productIds);
+  for await (const productIds of arrayToStream(deletedProductIds, 1000)) {
+    const deletionResult = await db.collection('Products').deleteMany({ _id: { $in: productIds } });
+    metrics.deletedCount += deletionResult.deletedCount;
+
+  }
+  logMetrics(dataRows.length - 1, metrics); // dataRows.length-1 because there is a new line at the end of file.
 }
 
 function logMetrics(numberOfProcessedRows, metrics) {
